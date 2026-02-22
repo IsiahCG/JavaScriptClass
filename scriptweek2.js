07a1cca837aa616d8fed56d01289377fb9916f57
IsiahCG <isiah.andrews1@smail.rasmussen.edu>
2026-02-21 23:50:07 -0600

    Fix variable name and typos in scriptweek2.js

--- scriptweek2.js
+++ scriptweek2.js
@@ -4,13 +4,13 @@ let Fname = prompt("Enter your first name:");
 alert("Welcome, " + Fname + "!");
 // Define the value of pi
 const piValue = 3.1415926;
-// Prompt the user to enter their favorite number and convert it to a number type
+// Prompt the user to enter their favorite number
 let myFavNum = prompt("Enter your favorite number:");
 // Convert the input to a number
-myFavnumber = Number(myFavNum);
-// Calculate the area of a circle using the formula A = πr^2
+myFavNum = Number(myFavNum);
+// Calculate the area of a circle using A = πr²
 let myArea = piValue * myFavNum * myFavNum;
 // Display the results to the user
-document.write("<p>Hello " + Fname + ", you entred " + myFavNum + " as your favorite number.</p>");
+document.write("<p>Hello " + Fname + ", you entered " + myFavNum + " as your favorite number.</p>");
 document.write("<p>If that was the radius of a circle, then the area of that circle would be " + myArea + ".</p>");
 document.write("<p>The value of pi is " + piValue + ".</p>");
