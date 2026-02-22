9d9ad789bd99e1d37149bed9f5ca0d31c9d78af5
IsiahCG <isiah.andrews1@smail.rasmussen.edu>
2026-02-21 23:38:08 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// JavaScript Program
+let Fname = prompt("Enter your first name:");
+// Display a welcome message using the user's first name
+alert("Welcome, " + Fname + "!");
+// Define the value of pi
+const piValue = 3.1415926;
+// Prompt the user to enter their favorite number and convert it to a number type
+let myFavNum = prompt("Enter your favorite number:");
+// Convert the input to a number
+myFavnumber = Number(myFavNum);
+// Calculate the area of a circle using the formula A = πr^2
+let myArea = piValue * myFavNum * myFavNum;
+// Display the results to the user
+document.write("<p>Hello " + Fname + ", you entred " + myFavNum + " as your favorite number.</p>");
+document.write("<p>If that was the radius of a circle, then the area of that circle would be " + myArea + ".</p>");
+document.write("<p>The value of pi is " + piValue + ".</p>");
